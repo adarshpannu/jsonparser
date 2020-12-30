fe44b90529ad34bd5b155a34de5c468d9da2a0e3
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-12-30 12:40:15 -0800

    Ok here we go

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,70 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+// JSON Parser (as if the world needs another one)
+
+#![allow(warnings)]
+
+pub struct JSONParser<'a> {
+    tokens: Vec<&'a str>
+}
+
+impl<'a> JSONParser<'a> {
+    fn parse(s: &str) -> JSONParser {
+        let tokens = Self::tokenize(s);
+        JSONParser { tokens }
     }
+
+    fn tokenize(s: &str) -> Vec<&str> {
+        s.split(|c| c == ' ' || c == '\n')
+            .filter(|&s| s.len() > 0)
+            .collect::<Vec<&str>>()
+    }
+}
+
+enum Token {
+    OpenBrace,
+    CloseBrace,
+    Identifier,
+    StringLiteral,
+    NumericLiteral,
+    Comma
+}
+
+// State machine
+// State + Token -> function?
+
+// Stack
+
+impl<'a> JSONParser<'a> {
+    fn parseValue() {
+
+    }
+
+    fn parseObject() {
+
+    }
+
+    fn parseNameValuePairs() {
+
+    }
+
+    fn parseNameValuePair() {
+
+    }
+
+    fn parseName() {
+
+    }
+
+}
+
+#[test]
+fn it_works() {
+    use JSONParser;
+
+    let jsonstr = r#"
+    {
+        name: "adarsh",
+        age: 53
+    }"#;
+
+    dbg!(JSONParser::tokenize(jsonstr));
 }
