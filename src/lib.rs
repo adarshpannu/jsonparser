2af9c842a1b55a704b56f0c74088ff8ad635ac4d
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-12-30 19:02:03 -0800

    More

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub struct JSONParser<'a> {
 
 #[derive(Debug)]
 enum JSONValue<'a> {
-    Object,
+    Object(Vec<(&'a str, JSONValue<'a>)>),
     Array(Vec<JSONValue<'a>>),
     StringLiteral(&'a str),
     NumericLiteral(&'a str),
@@ -21,7 +21,15 @@ enum JSONValue<'a> {
 }
 
 #[derive(Debug)]
-struct ParseError;
+struct ParseError<'a> {
+    token: &'a str,
+}
+
+impl<'a> ParseError<'a> {
+    fn new(token: &'a str) -> ParseError {
+        ParseError { token }
+    }
+}
 
 impl<'a> JSONParser<'a> {
     fn new(s: &str) -> JSONParser {
@@ -29,7 +37,7 @@ impl<'a> JSONParser<'a> {
         dbg!(&tokens);
         JSONParser {
             tokens,
-            token_ix: RefCell::new(0)
+            token_ix: RefCell::new(0),
         }
     }
 
@@ -50,66 +58,111 @@ impl<'a> JSONParser<'a> {
         if *token_ix <= tokens.len() {
             let token = tokens[*token_ix];
             *token_ix += 1;
+            println!("NEXT TOKEN: {:?}", token);
             Some(token)
         } else {
             None
         }
     }
 
-    fn parseValue(&self) -> Result<JSONValue, ParseError> {
+    fn unread_token(&self) {
+        let mut token_ix = self.token_ix.borrow_mut();
+
+        if *token_ix > 0 {
+            *token_ix -= 1;
+        } else {
+            panic!("Internal error: Already at beginning of token stream, cannot unread another token.")
+        }
+    }
+
+    fn peek_token(&self) -> Option<&str> {
         let token = self.next_token();
-        match token.unwrap() {
-            "{" => self.parseObject(),
+        self.unread_token();
+        token
+    }
+
+    fn is_string(s: &str) -> bool {
+        return s.len() > 0 && s.starts_with("\"") && s.ends_with("\"")
+    }
+
+    fn is_number(s: &str) -> bool {
+        s.parse::<f64>().is_ok()
+    }
+
+    fn parseValue(&self) -> Result<JSONValue, ParseError> {
+        let token = self.peek_token();
+        let token = token.unwrap();
+
+        match token {
+            //"{" => self.parseObject(),
             "[" => self.parseArray(),
             "\"true\"" => self.parseTrue(),
             "\"false\"" => self.parseFalse(),
             "\"null\"" => self.parseNull(),
-            _ => Err(ParseError),
+            _ => {
+                if Self::is_string(token) {
+                    self.parseString()
+                } else if Self::is_number(token) {
+                    self.parseNumber()
+                } else {
+                    Err(ParseError::new(token))
+                }
+            }
         }
     }
 
     fn parseArray(&self) -> Result<JSONValue, ParseError> {
         let mut array: Vec<JSONValue> = Vec::new();
+        self.next_token(); // Eat "["
 
+        let mut seen_comma = false;
         loop {
             let token = self.next_token();
             match token {
-                None => return Err(ParseError),
+                None => return Err(ParseError::new("Unexpected end of input")),
                 Some("]") => return Ok(JSONValue::Array(array)),
+                Some(",") => {
+                    if seen_comma {
+                        return Err(ParseError::new(","));
+                    }
+                    seen_comma = true;
+                }
                 _ => {
+                    if array.len() > 0 && seen_comma == false {
+                        return Err(ParseError::new(","));
+                    }
+                    seen_comma = false;
+                    self.unread_token();
                     let jv = self.parseValue()?;
                     array.push(jv);
-                },
+                }
             }
         }
     }
 
-    fn parseObject(&self) -> Result<JSONValue, ParseError> {
-        Ok(JSONValue::Null)
-    }
-
     fn parseTrue(&self) -> Result<JSONValue, ParseError> {
+        self.next_token();
         Ok(JSONValue::True)
     }
 
     fn parseFalse(&self) -> Result<JSONValue, ParseError> {
+        self.next_token();
         Ok(JSONValue::False)
     }
 
     fn parseNull(&self) -> Result<JSONValue, ParseError> {
+        self.next_token();
         Ok(JSONValue::Null)
     }
 
-    fn parseNameValuePairs(&self) -> Result<JSONValue, ParseError> {
-        Ok(JSONValue::Null)
+    fn parseString(&self) -> Result<JSONValue, ParseError> {
+        let token = self.next_token().unwrap();
+        Ok(JSONValue::StringLiteral(token))
     }
 
-    fn parseNameValuePair(&self) -> Result<JSONValue, ParseError> {
-        Ok(JSONValue::Null)
-    }
-
-    fn parseName(&self) -> Result<JSONValue, ParseError> {
-        Ok(JSONValue::Null)
+    fn parseNumber(&self) -> Result<JSONValue, ParseError> {
+        let token = self.next_token().unwrap();
+        Ok(JSONValue::NumericLiteral(token))
     }
 }
 
@@ -124,7 +177,7 @@ fn it_works() {
     }"#;
 
     let jsonstr = r#"
-    "true"
+    [ "true" , "false" , [ "null" ] , "adarsh" , 1.32 ]
     "#;
 
     let mut jp = JSONParser::new(jsonstr);
